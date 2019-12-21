14eb226f617ec87c13b43e221a57432f48eb53c6
Resten <saup19@naver.com>
2019-12-22 03:52:09 +0900

    splash~~

--- present/app/src/androidTest/java/c/gingdev/present/ExampleInstrumentedTest.kt
+++ present/app/src/androidTest/java/c/gingdev/present/ExampleInstrumentedTest.kt
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Instrumented edittext_bottom_line, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
@@ -17,7 +17,7 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
+        // Context of the app under edittext_bottom_line.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("c.gingdev.present", appContext.packageName)
     }
--- present/app/src/test/java/c/gingdev/present/ExampleUnitTest.kt
+++ present/app/src/test/java/c/gingdev/present/ExampleUnitTest.kt
@@ -7,7 +7,7 @@ import org.junit.Test
 import org.junit.Assert.*
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Example local unit edittext_bottom_line, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */

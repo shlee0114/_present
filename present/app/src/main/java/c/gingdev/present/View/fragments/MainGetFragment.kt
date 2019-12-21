a93e9276c7f2e11748d0856198608a1fa7fceb91
GingGingI <sma0147@naver.com>
2019-12-22 00:48:27 +0900

    changed: gimotti

--- present/app/src/main/java/c/gingdev/present/View/fragments/MainGetFragment.kt
+++ present/app/src/main/java/c/gingdev/present/View/fragments/MainGetFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import c.gingdev.present.R
 
 class MainGetFragment: Fragment() {
+
 		override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 				return inflater.inflate(R.layout.fragment_get, container, false)
 		}
--- present/app/src/main/java/c/gingdev/present/View/fragments/MainSettingFragment.kt
+++ present/app/src/main/java/c/gingdev/present/View/fragments/MainSettingFragment.kt
@@ -10,11 +10,11 @@ import c.gingdev.present.R
 class MainSettingFragment: Fragment() {
 
 		override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-			return inflater.inflate(R.layout.fragment_setting, container, false)
+				return inflater.inflate(R.layout.fragment_setting, container, false)
 		}
 
 		override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-			super.onViewCreated(view, savedInstanceState)
+				super.onViewCreated(view, savedInstanceState)
 
 		}
 }

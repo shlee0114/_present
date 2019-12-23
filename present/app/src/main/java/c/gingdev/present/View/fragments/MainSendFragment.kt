98794011b312be6a5b93e0e9fc49fc438a508679
tlgj255 <tlgj255@naver.com>
2019-12-23 23:30:36 +0900

    ..

--- present/app/src/main/java/c/gingdev/present/View/fragments/MainSendFragment.kt
+++ present/app/src/main/java/c/gingdev/present/View/fragments/MainSendFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.transition.Visibility
 import c.gingdev.present.Model.permissionModels
 import c.gingdev.present.R
 import c.gingdev.present.Utils.etc.permissionChecker
@@ -16,9 +20,26 @@ import c.gingdev.present.View.MainActivity
 
 class MainSendFragment: Fragment() {
 
+    var imageView: ImageView? = null
+    var picture:LinearLayout? = null
 		override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 			val inflater = inflater.inflate(R.layout.fragment_send, container, false)
 			val btnInsertImage = inflater.findViewById<Button>(R.id.addPic)
+            val btnNextLv = inflater.findViewById<Button>(R.id.nextLv)
+            val editName = inflater.findViewById<EditText>(R.id.name)
+            val editCard = inflater.findViewById<EditText>(R.id.sendText)
+            val btnCancel = inflater.findViewById<Button>(R.id.btnCancle)
+            picture = inflater.findViewById(R.id.picture)
+
+            btnCancel.setOnClickListener{
+                picture!!.visibility = View.GONE
+            }
+
+            btnNextLv.setOnClickListener{
+                val send = sendPresntUtil()
+                send.sendUtil(editName.toString(),"picture",editCard.toString(),"", imageView!!)
+            }
+
             btnInsertImage.setOnClickListener{
                 val intent = Intent(Intent.ACTION_PICK)
                 intent.type = "image/*"
@@ -30,12 +51,12 @@ class MainSendFragment: Fragment() {
 				return inflater
 
 		}
-    var imageView: ImageView? = null
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         imageView!!.setImageURI(data!!.data)
-        val sent = sendPresntUtil()
+        picture!!.visibility =View.VISIBLE
     }
 
 		override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
